caae6739f286c9a4e2dec2db90a153a54cdfab2e
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2022-08-07 16:28:24 +0200

    Get rid of T A B s

--- .config/nvim/after/plugin/color.lua
+++ .config/nvim/after/plugin/color.lua
@@ -1,13 +1,3 @@
---[[
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_transparent = true
-
-
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-]]
-
 vim.g.gruvbox_contrast_dark = 'hard'
 vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_transparent = true
@@ -15,4 +5,4 @@ vim.g.gruvbox_invert_selection = '0'
 vim.opt.background = "dark"
 
 vim.cmd[[colorscheme tokyonight]]
--- vim.cmd[[colorscheme nightfox]]
+
