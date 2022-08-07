a53fc5b339c8c1ec1e8a16f0b30c5d36614947b1
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2022-08-07 17:03:14 +0200

    More keymaps /o/

--- .config/nvim/lua/kublaios/remap.lua
+++ .config/nvim/lua/kublaios/remap.lua
@@ -1,3 +1,20 @@
 local nnoremap = require("kublaios.keymap").nnoremap
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 
+vim.cmd([[
+nnoremap <leader>h :wincmd h<CR>
+nnoremap <leader>j :wincmd j<CR>
+nnoremap <leader>k :wincmd k<CR>
+nnoremap <leader>l :wincmd l<CR>
+
+nnoremap <leader>= :vertical resize +5<CR>
+nnoremap <leader>- :vertical resize -5<CR>
+nnoremap <leader>rp :resize 100<CR>
+
+nnoremap <leader>u :UndotreeShow<CR>
+
+nnoremap <leader>phw :h <C-R>=expand("<cword>")<CR><CR>
+
+nnoremap <leader>q :q<CR>
+nnoremap <leader>Q :q!<CR>
+]])
