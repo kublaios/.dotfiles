7e30b406be024ef703f8e926ba06950d9df178d8
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2023-10-25 14:20:17 +0200

    Make terminal colors nice again.

--- .config/nvim/after/plugin/color.lua
+++ .config/nvim/after/plugin/color.lua
@@ -1,8 +1,15 @@
-vim.g.gruvbox_contrast_dark = 'hard'
-vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_transparent = true
-vim.g.gruvbox_invert_selection = '0'
-vim.opt.background = "dark"
+-- vim.cmd [[ set background=dark ]]
+-- vim.cmd [[ colorscheme tokyonight ]]
+-- vim.g.tokyonight_transparent_sidebar = true
+-- vim.g.tokyonight_transparent = true
 
-vim.cmd[[colorscheme tokyonight]]
+-- set statusline color to black
+vim.cmd [[ hi StatusLine ctermfg=233 ]]
+-- set gutter color to red
+vim.cmd [[ hi SignColumn ctermbg=233 ]]
 
+vim.cmd [[ hi LineNr ctermfg=darkgray ctermbg=NONE ]]
+vim.cmd [[ hi DiffAdd ctermfg=NONE ctermbg=NONE ]]
+vim.cmd [[ hi DiffChange ctermfg=NONE ctermbg=NONE ]]
+vim.cmd [[ hi DiffDelete ctermfg=NONE ctermbg=NONE ]]
+vim.cmd [[ hi DiffText ctermfg=NONE ctermbg=NONE ]]
--- .config/nvim/lua/kublaios/init.lua
+++ .config/nvim/lua/kublaios/init.lua
@@ -20,6 +20,7 @@ let g:syntastic_always_populate_loc_list = 1
 let g:syntastic_auto_loc_list = 1
 let g:syntastic_check_on_open = 1
 let g:syntastic_check_on_wq = 0
+let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro'
 
 " swift.vim
 " swiftpm is disabled because it slows things down tremendously
