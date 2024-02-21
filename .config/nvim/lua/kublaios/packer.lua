6232118798dfd23a9ea2de96e716da654a7e0e76
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2024-02-21 23:36:34 +0100

    Update Telescope dependency version in Nvim settings.

--- .config/nvim/lua/kublaios/packer.lua
+++ .config/nvim/lua/kublaios/packer.lua
@@ -7,7 +7,7 @@ return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use {'neoclide/coc.nvim', branch = 'release'}
-  use {'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = {{'nvim-lua/plenary.nvim'}}}
+  use {'nvim-telescope/telescope.nvim', tag = '0.1.4', requires = {{'nvim-lua/plenary.nvim'}}}
   use 'github/copilot.vim'
   use 'folke/tokyonight.nvim'
 end)
