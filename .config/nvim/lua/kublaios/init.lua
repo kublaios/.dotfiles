8f2cc64ef442ad2ca6da9d80ff6399e9f2c0c12e
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2024-02-26 09:11:58 +0100

    Remove unused neovim config.

--- .config/nvim/lua/kublaios/init.lua
+++ .config/nvim/lua/kublaios/init.lua
@@ -1,29 +1,14 @@
 require("kublaios.set")
 require("kublaios.packer")
 require("kublaios.remap")
-require("kublaios.coc")
 
 vim.wo.relativenumber = true
 
 vim.cmd([[
+
 " Pathogen
 execute pathogen#infect()
 syntax on
 filetype plugin indent on
 
-" Syntactic
-set statusline+=%#warningmsg#
-set statusline+=%{SyntasticStatuslineFlag()}
-set statusline+=%*
-
-let g:syntastic_always_populate_loc_list = 1
-let g:syntastic_auto_loc_list = 1
-let g:syntastic_check_on_open = 1
-let g:syntastic_check_on_wq = 0
-let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro'
-
-" swift.vim
-" swiftpm is disabled because it slows things down tremendously
-" let g:syntastic_swift_checkers = ['swiftpm', 'swiftlint']
-let g:syntastic_swift_checkers = ['swiftlint']
 ]])
--- .config/nvim/lua/kublaios/packer.lua
+++ .config/nvim/lua/kublaios/packer.lua
@@ -6,7 +6,6 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use {'neoclide/coc.nvim', branch = 'release'}
   use {'nvim-telescope/telescope.nvim', tag = '0.1.4', requires = {{'nvim-lua/plenary.nvim'}}}
   use 'github/copilot.vim'
   use 'folke/tokyonight.nvim'
