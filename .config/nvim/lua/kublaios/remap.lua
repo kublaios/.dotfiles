73cd55994ac3cabf0c5a3c2335d105668d40b4d5
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2024-06-13 18:11:22 +0200

    Add key bindings for lazygit and returning to netrw.

--- .config/nvim/lua/kublaios/remap.lua
+++ .config/nvim/lua/kublaios/remap.lua
@@ -1,6 +1,6 @@
 local nnoremap = require("kublaios.keymap").nnoremap
 local xnoremap = require("kublaios.keymap").xnoremap
-nnoremap("<leader>pv", "<cmd>Ex<CR>")
+nnoremap("<leader>q", "<cmd>Ex<CR>")
 xnoremap("<leader>p", "\"_dP")
 
 vim.cmd([[
@@ -9,18 +9,19 @@ nnoremap <leader>j :wincmd j<CR>
 nnoremap <leader>k :wincmd k<CR>
 nnoremap <leader>l :wincmd l<CR>
 
-nnoremap <leader>= :vertical resize +5<CR>
-nnoremap <leader>- :vertical resize -5<CR>
+nnoremap <leader>= :vertical resize +10<CR>
+nnoremap <leader>- :vertical resize -10<CR>
 nnoremap <leader>rp :resize 100<CR>
 
 nnoremap <leader>u :UndotreeShow<CR>
 
 nnoremap <leader>phw :h <C-R>=expand("<cword>")<CR><CR>
 
-nnoremap <leader>q :q<CR>
 nnoremap <leader>Q :q!<CR>
 
 vnoremap <leader>y "*y<CR>
 
 nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+
+nnoremap <silent> <leader>gg :LazyGit<CR>
 ]])
