d7a7bf92ad9185bf53606590356bbe8c880f7968
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2024-06-29 15:33:45 +0200

    Improve vim view.

--- .config/nvim/lua/kublaios/set.lua
+++ .config/nvim/lua/kublaios/set.lua
@@ -1,9 +1,13 @@
--- vim.opt.guicursor = ""
+vim.g.mapleader = " "
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+-- highlight the current line
+vim.opt.cursorline = true
+-- always show the sign column to avoid screen flickering for lsp errors
+vim.opt.signcolumn = "yes"
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -14,10 +18,9 @@ vim.opt.incsearch = true
 
 vim.opt.smartindent = true
 -- vim.opt.termguicolors = true
+-- vim.opt.guicursor = ""
 
 vim.opt.wrap = false
-
-vim.g.mapleader = " "
 
 vim.o.timeout = true
 vim.o.timeoutlen = 300
