a514343be50e286cf90c95e77b4974bf7131e420
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2024-07-15 16:28:48 +0200

    Remove config that silently breaks which-key.

--- .config/nvim/lua/kublaios/init.lua
+++ .config/nvim/lua/kublaios/init.lua
@@ -37,13 +37,6 @@ wk.setup {
         separator = "➜", -- symbol used between a key and it's label
         group = "+", -- symbol prepended to a group
     },
-    win = {
-        bo = "none",          -- none, single, double, shadow
-        margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-        padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-        winblend = 0,             -- value between 0-100 0 for fully opaque and 100 for fully transparent
-        zindex = 1000,            -- positive value to position WhichKey above other floating windows.
-    },
 }
 wk.add({
     { "<leader>f", group = "flash" },
