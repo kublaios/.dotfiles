8eb5fa88acc9206b2821ee0c25cec225f87983d6
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2024-07-15 16:06:35 +0200

    Pin the version or pay the price (same thing).

--- .config/nvim/lua/kublaios/init.lua
+++ .config/nvim/lua/kublaios/init.lua
@@ -32,19 +32,13 @@ wk.setup {
             g = true,            -- bindings for prefixed with g
         },
     },
-    operators = { gc = "Comments" },
     icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
         separator = "➜", -- symbol used between a key and it's label
         group = "+", -- symbol prepended to a group
     },
-    popup_mappings = {
-        scroll_down = "<c-d>", -- binding to scroll down inside the popup
-        scroll_up = "<c-u>",   -- binding to scroll up inside the popup
-    },
-    window = {
-        border = "none",          -- none, single, double, shadow
-        position = "bottom",      -- bottom, top
+    win = {
+        bo = "none",          -- none, single, double, shadow
         margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
         padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
         winblend = 0,             -- value between 0-100 0 for fully opaque and 100 for fully transparent
