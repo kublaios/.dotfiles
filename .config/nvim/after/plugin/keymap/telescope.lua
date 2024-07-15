372f5d05eb9e117f5029bbb6205258c5ed1e2e83
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2024-07-15 15:36:35 +0200

    Remove conflict with write shortcut.

--- .config/nvim/after/plugin/keymap/telescope.lua
+++ .config/nvim/after/plugin/keymap/telescope.lua
@@ -80,7 +80,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
         -- Fuzzy find all the symbols in your current workspace.
         --  Similar to document symbols, except searches over your entire project.
-        lspMap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+        lspMap("<leader>Ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
